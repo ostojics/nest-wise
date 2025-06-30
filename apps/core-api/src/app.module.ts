e41caabe7660a440a83fb60d258104d88ca6fc07
ostojics <ostojic.dev@gmail.com>
2025-06-30 19:17:41 +0200

    init config service

--- apps/core-api/src/app.module.ts
+++ apps/core-api/src/app.module.ts
@@ -2,9 +2,11 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {LoggerModule} from 'pino-nestjs';
+import {ConfigModule} from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
-  imports: [LoggerModule.forRoot()],
+  imports: [ConfigModule.forRoot({cache: true, load: [appConfig]}), LoggerModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -3,9 +3,13 @@ import {AppModule} from './app.module';
 import {Logger} from 'pino-nestjs';
 import {VERSION_NEUTRAL, VersioningType} from '@nestjs/common';
 import {NestExpressApplication} from '@nestjs/platform-express';
+import {ConfigService} from '@nestjs/config';
+import {GlobalConfig} from './config/config.type';
+import {AppConfig, AppConfigName} from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {bufferLogs: true});
+  const configService = app.get(ConfigService<GlobalConfig>);
 
   app.useLogger(app.get(Logger));
   app.enableVersioning({
@@ -14,7 +18,8 @@ async function bootstrap() {
   });
   app.useBodyParser('json', {limit: '10mb'});
 
-  await app.listen(process.env.PORT ?? 8080);
+  const {port} = configService.getOrThrow<AppConfig>(AppConfigName);
+  await app.listen(port);
 }
 
 void bootstrap();
