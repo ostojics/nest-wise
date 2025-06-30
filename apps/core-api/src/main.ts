644ba7565062d4e72328d996690575223a7cde8b
ostojics <ostojic.dev@gmail.com>
2025-06-30 18:58:36 +0200

    add versioning and body parser

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,11 +1,20 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {Logger} from 'pino-nestjs';
+import {VERSION_NEUTRAL, VersioningType} from '@nestjs/common';
+import {NestExpressApplication} from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {bufferLogs: true});
-  await app.listen(process.env.PORT ?? 8080);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {bufferLogs: true});
+
   app.useLogger(app.get(Logger));
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: VERSION_NEUTRAL,
+  });
+  app.useBodyParser('json', {limit: '10mb'});
+
+  await app.listen(process.env.PORT ?? 8080);
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-bootstrap();
+
+void bootstrap();
