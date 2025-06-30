87611af6679dde5da26ecff76c84c3fd27185c66
ostojics <ostojic.dev@gmail.com>
2025-06-30 18:51:12 +0200

    setup logger

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,9 +1,11 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
+import {Logger} from 'pino-nestjs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {bufferLogs: true});
   await app.listen(process.env.PORT ?? 8080);
+  app.useLogger(app.get(Logger));
 }
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
