65e5855322fdcb02592b92e33884418904460f91
ostojics <ostojic.dev@gmail.com>
2025-07-05 21:02:27 +0200

    include database config

--- apps/core-api/src/app.module.ts
+++ apps/core-api/src/app.module.ts
@@ -7,10 +7,11 @@ import {ConfigModule, ConfigService} from '@nestjs/config';
 import {ThrottlerModule} from '@nestjs/throttler';
 import {throttlerConfig, throttlerFactory} from './config/throttler.config';
 import {appConfig} from './config/app.config';
+import {databaseConfig} from './config/database.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({cache: true, load: [appConfig, throttlerConfig]}),
+    ConfigModule.forRoot({cache: true, load: [appConfig, throttlerConfig, databaseConfig]}),
     LoggerModule.forRoot(),
     // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     ThrottlerModule.forRootAsync({
