6a96b69eb036e7561ba72240879ea22a8fe16a82
ostojics <ostojic.dev@gmail.com>
2025-07-12 21:48:12 +0200

    get household accounts endpoint

--- apps/core-api/src/households/households.controller.ts
+++ apps/core-api/src/households/households.controller.ts
@@ -1,7 +1,17 @@
-import {Controller} from '@nestjs/common';
+import {Controller, Get, Param, UseGuards} from '@nestjs/common';
 import {HouseholdsService} from './households.service';
+import {AuthGuard} from 'src/common/guards/auth.guard';
 
-@Controller('households')
+@Controller({
+  version: '1',
+  path: 'households',
+})
 export class HouseholdsController {
   constructor(private readonly householdsService: HouseholdsService) {}
+
+  @UseGuards(AuthGuard)
+  @Get(':id/accounts')
+  async getAccountsByHouseholdId(@Param('id') id: string) {
+    return await this.householdsService.findAccountsByHouseholdId(id);
+  }
 }
