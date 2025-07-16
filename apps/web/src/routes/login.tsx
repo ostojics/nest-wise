09b8060d9dcdefc443596c5f1d2e83f100873f3e
ostojics <ostojic.dev@gmail.com>
2025-07-16 20:11:42 +0200

    adjust styling

--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -2,5 +2,9 @@ import LoginPage from '@/modules/auth/components/login-form';
 import {createFileRoute} from '@tanstack/react-router';
 
 export const Route = createFileRoute('/login')({
-  component: LoginPage,
+  component: () => (
+    <section className="flex items-center justify-center h-screen w-screen">
+      <LoginPage />
+    </section>
+  ),
 });
