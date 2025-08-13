8f22a5cfecfeb210205644493701180a1dc91427
ostojics <ostojic.dev@gmail.com>
2025-08-13 22:33:59 +0200

    add household id by default in the search query for transactions table

--- apps/web/src/modules/transactions/components/transactions-page.tsx
+++ apps/web/src/modules/transactions/components/transactions-page.tsx
@@ -4,12 +4,15 @@ import TransactionsPagination from './transactions-pagination';
 import {TransactionsTable} from './transactions-table';
 import TransactionsTableActions from './transactions-table-actions';
 import {TransactionContract} from '@maya-vault/contracts';
+import {useGetMe} from '@/modules/auth/hooks/useGetMe';
 
 const fallbackTransactions: TransactionContract[] = [];
 
 const TransactionsPage = () => {
   const search = useSearch({from: '/__pathlessLayout/transactions'});
-  const {data} = useGetTransactions({search});
+  const {data: me} = useGetMe();
+  const householdId = me?.householdId;
+  const {data} = useGetTransactions({search: {...search, householdId}});
 
   return (
     <section className="p-4 flex flex-col h-full">
