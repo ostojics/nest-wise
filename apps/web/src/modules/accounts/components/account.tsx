1f72b86aa91ffd7fa047c2b6ec58aed617dc2473
ostojics <ostojic.dev@gmail.com>
2025-09-06 20:57:45 +0200

    accounts icon responsivity

--- apps/web/src/modules/accounts/components/account.tsx
+++ apps/web/src/modules/accounts/components/account.tsx
@@ -27,7 +27,7 @@ const Account: React.FC<AccountProps> = ({account}) => {
         <div className="flex flex-col items-start gap-3 md:gap-0 md:flex-row md:items-center justify-between">
           <div className="flex items-center gap-3">
             {IconComponent && (
-              <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-accent/50">
+              <div className="flex h-10 min-w-10 items-center justify-center rounded-lg bg-accent/50">
                 <IconComponent className="h-5 w-5 text-accent-foreground" />
               </div>
             )}
