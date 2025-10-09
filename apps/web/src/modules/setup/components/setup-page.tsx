93e3fec5a97704e90df6ab300865f9d6cb631c78
ostojics <ostojic.dev@gmail.com>
2025-10-09 22:07:09 +0200

    remove leave alert (#132)

--- apps/web/src/modules/setup/components/setup-page.tsx
+++ apps/web/src/modules/setup/components/setup-page.tsx
@@ -1,15 +1,10 @@
 import {useSetupContext} from '../hooks/use-setup';
-import {useBeforeUnload} from '@/hooks/use-before-unload';
 import Step1 from './step-1';
 import Step2 from './step-2';
 import Stepper from './stepper';
 
 const SetupPage = () => {
   const {currentStep} = useSetupContext();
-  useBeforeUnload(
-    true,
-    'Your setup is in progress. If you leave this page, your progress may be lost. Please use the link you received to resume.',
-  );
 
   return (
     <section className="flex flex-col items-center justify-center h-screen">
