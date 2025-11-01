b8eb75b56bb23771da77c9809b767060f2622331
ostojics <ostojic.dev@gmail.com>
2025-11-01 21:53:57 +0100

    improve error handling

--- apps/web/src/modules/accounts/hooks/use-activate-account-mutation.ts
+++ apps/web/src/modules/accounts/hooks/use-activate-account-mutation.ts
@@ -2,6 +2,8 @@ import {useMutation, useQueryClient} from '@tanstack/react-query';
 import {activateAccount} from '@/modules/api/accounts-api';
 import {toast} from 'sonner';
 import {queryKeys} from '@/modules/api/query-keys';
+import {HTTPError} from 'ky';
+import {ErrorResponse} from '@nest-wise/contracts';
 
 export const useActivateAccountMutation = (accountId: string) => {
   const queryClient = useQueryClient();
@@ -12,8 +14,16 @@ export const useActivateAccountMutation = (accountId: string) => {
       void queryClient.invalidateQueries({queryKey: queryKeys.accounts.all()});
       toast.success('Račun je aktiviran.');
     },
-    onError: () => {
-      toast.error('Došlo je do greške. Pokušajte ponovo.');
+    onError: async (error) => {
+      const typedError = error as HTTPError<ErrorResponse>;
+      const err = await typedError.response.json();
+
+      if (err.message) {
+        toast.error(err.message);
+        return;
+      }
+
+      toast.error('Došlo je do neočekivane greške');
     },
   });
 };
