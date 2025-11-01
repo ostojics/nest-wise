2b9ac6d3e91f71c8c48d8e46bb0315e5844a5666
ostojics <ostojic.dev@gmail.com>
2025-11-01 21:33:15 +0100

    add migration and adjust switch component

--- apps/core-api/src/config/database.config.ts
+++ apps/core-api/src/config/database.config.ts
@@ -23,6 +23,7 @@ import {AddScheduledTransactionExecutions1760300000001} from 'src/migrations/176
 import {ScheduledTransactionRule} from 'src/scheduled-transactions/scheduled-transaction-rule.entity';
 import {AddCategoryDescription1760998944930} from 'src/migrations/1760998944930-AddCategoryDescription';
 import {DropSavings1761000000000} from 'src/migrations/1761000000000-DropSavings';
+import {AddAccountIsActive1761949055000} from 'src/migrations/1761949055000-AddAccountIsActive';
 
 export const DatabaseConfigName = 'database';
 
@@ -66,6 +67,7 @@ export function getConfig(): DatabaseConfig {
       DropSavings1761000000000,
       AddScheduledTransactionRules1760300000000,
       AddScheduledTransactionExecutions1760300000001,
+      AddAccountIsActive1761949055000,
     ],
     migrationsRun: true,
     extra: {
