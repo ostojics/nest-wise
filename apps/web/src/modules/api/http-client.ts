764d05a0c04eb65d44d22865f6f3d88daf5f63be
ostojics <ostojic.dev@gmail.com>
2025-11-08 22:03:20 +0100

    remove exception tracking from http client

--- apps/web/src/modules/api/http-client.ts
+++ apps/web/src/modules/api/http-client.ts
@@ -9,25 +9,10 @@ const httpClient = ky.create({
 const extended = httpClient.extend({
   hooks: {
     afterResponse: [
-      async (_request, _options, response) => {
+      (_request, _options, response) => {
         if (isPublicRoute(window.location.pathname)) return response;
 
         const {status} = response;
-
-        // Log HTTP errors to PostHog
-        if (!response.ok) {
-          const {default: posthog} = await import('posthog-js');
-          posthog.captureException(new Error(`HTTP ${status}: ${response.url}`), {
-            context: {
-              feature: 'ApiClient',
-            },
-            meta: {
-              status,
-              url: response.url,
-              method: _request.method,
-            },
-          });
-        }
 
         if (status === 401 || status === 403) {
           window.location.href = '/login';
